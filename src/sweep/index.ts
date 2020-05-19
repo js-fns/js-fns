5bf31bc0f18386c314da710bd6ff3d72eadf48cc
Sasha Koss <koss@nocorp.me>
2020-05-19 21:45:21 +0400

    Make sweep return type more specific

--- src/sweep/index.ts
+++ src/sweep/index.ts
@@ -1,3 +1,6 @@
-export default function sweep<T>(array: Array<T>): Array<T> {
-  return array.filter((item) => item != null)
+export default function sweep<
+  ItemType,
+  ReturnType extends Array<Exclude<ItemType, null | undefined>>
+>(array: Array<ItemType>): ReturnType {
+  return array.filter((item) => item != null) as ReturnType
 }
--- src/sweep/test.ts
+++ src/sweep/test.ts
@@ -3,10 +3,19 @@ import sweep from '.'
 
 describe('sweep', () => {
   it('removes null and undefined values from the array', () => {
-    assert.deepEqual(sweep([null, 1, null, undefined, 2, null, 3, undefined]), [
+    const result = sweep([
+      null,
       1,
+      null,
+      undefined,
       2,
+      null,
       3,
+      undefined,
+      0,
+      false,
+      '',
     ])
+    assert.deepEqual(result, [1, 2, 3, 0, false, ''])
   })
 })
