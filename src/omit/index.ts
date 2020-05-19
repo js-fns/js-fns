7998d8087d396716d3a40f122d285da5c7cb6074
Sasha Koss <koss@nocorp.me>
2020-05-19 22:19:32 +0400

    Update omit keys type

--- src/omit/index.ts
+++ src/omit/index.ts
@@ -1,8 +1,8 @@
 export default function omit<
   ObjectType extends {},
   Key extends keyof ObjectType
->(object: ObjectType, keys: Key | Key[]): Omit<ObjectType, Key> {
+>(object: ObjectType, keys: Key | [Key, ...Key[]]): Omit<ObjectType, Key> {
   const clone = { ...object }
-  ;(Array.isArray(keys) ? keys : [keys]).forEach(key => delete clone[key])
+  ;(Array.isArray(keys) ? keys : [keys]).forEach((key) => delete clone[key])
   return clone
 }
