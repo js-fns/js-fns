45ec6ea2d8b8d749d76e637fa7be73bb8dc07219
Sasha Koss <koss@nocorp.me>
2020-05-19 22:14:29 +0400

    Save some bytes in pick, fix the mutaion test

--- src/pick/index.ts
+++ src/pick/index.ts
@@ -1,12 +1,14 @@
 export default function pick<
   ObjectType extends {},
-  Key extends keyof ObjectType
->(object: ObjectType, keys: [Key, ...Key[]]): Pick<ObjectType, Key> {
+  Key extends keyof ObjectType,
+  ReturnType extends Pick<ObjectType, Key>
+>(object: ObjectType, keys: [Key, ...Key[]]): ReturnType {
   return keys.reduce((result, key) => {
     if (key in object) {
       result[key] = object[key]
     }
-
     return result
-  }, Object.create(null))
+    // NOTE: Figure out why when we use ReturnType instead of the type below,
+    // it shows a type error.
+  }, {} as { [key in Key]: ObjectType[key] }) as ReturnType
 }
--- src/pick/test.ts
+++ src/pick/test.ts
@@ -1,26 +1,27 @@
 import assert from 'assert'
 import pick from '.'
 
-describe('pick', function() {
+describe('pick', () => {
   const obj = {
     a: 1,
     b: 2,
-    c: 3
+    c: 3,
   }
 
-  it('picks only required keys from object', function() {
+  it('picks only required keys from object', () => {
     const result = pick(obj, ['a', 'b'])
     assert.deepEqual(result, { a: 1, b: 2 })
   })
 
-  it('returns empty object if none of the keys are found', function() {
+  it('returns empty object if none of the keys are found', () => {
     // @ts-expect-error
     const result = pick(obj, ['nope', 'nah'])
     assert.deepEqual(result, {})
   })
 
-  it('does not change orignal object', function() {
-    const result = pick(obj, ['a', 'b'])
-    assert.deepEqual(obj, { a: 1, b: 2, c: 3 })
+  it('does not change orignal object', () => {
+    const clone = { ...obj }
+    pick(obj, ['a', 'b'])
+    assert.deepEqual(obj, clone)
   })
 })
