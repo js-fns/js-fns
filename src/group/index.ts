652e3550f4ad22ad8d7af2cbd8e81d5a386499d6
Sasha Koss <koss@nocorp.me>
2020-05-19 18:51:35 +0400

    Export Groups type from group function

--- src/group/index.ts
+++ src/group/index.ts
@@ -1,12 +1,12 @@
-type Group<ItemType> = {
+export type Groups<ItemType> = {
   [key: string]: ItemType[]
 }
 
 export default function group<ItemType, GroupIdType extends string | number>(
   arr: ItemType[],
   getGroupId: (item: ItemType, index: number) => GroupIdType
-): Group<ItemType> {
-  return arr.reduce<Group<ItemType>>((acc, item, index) => {
+): Groups<ItemType> {
+  return arr.reduce<Groups<ItemType>>((acc, item, index) => {
     const groupId = getGroupId(item, index)
     acc[groupId] = acc[groupId] || []
     acc[groupId].push(item)
