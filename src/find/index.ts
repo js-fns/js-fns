2a6cb9d743e54f27e14292ea5da082e4b2df74c1
Eduard Tsech <edtsech@gmail.com>
2020-05-21 15:04:05 +0200

    Add find

---
+++ src/find/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Returns the first matching element.
+ *
+ * @param array - The array to find element in
+ * @param matcher - The function that returns true if element satisfies a condition
+ * @returns An element that satisfies the condition
+ *
+ * @public
+ */
+export default function find<ElementType>(
+  array: Array<ElementType>,
+  matcher: (element: ElementType) => boolean
+): ElementType | undefined {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i]
+
+    if (matcher(element)) {
+      return element
+    }
+  }
+}
