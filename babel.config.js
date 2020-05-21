07b05cd81fe5a4fd1aa180868a4b69d3b1544196
Sasha Koss <koss@nocorp.me>
2020-05-21 20:48:14 +0400

    Target IE 11

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript',
-  ],
+  presets: ['@babel/preset-env', '@babel/preset-typescript'],
   env: {
     test: {
       presets: ['power-assert'],
