9c8e844738457738d43515aadf39a0a1a30b874d
Tatiana Shepeleva <tati.shep@gmail.com>
2020-05-22 12:18:47 +0400

    Add findLast function for array (#18)

--- src/find/test.ts
+++ src/find/test.ts
@@ -5,10 +5,10 @@ describe('find', function () {
   var array = [1, 2, 3]
 
   it('finds first element by condition', function () {
-    assert(find(array, (i) => i === 2) === 2)
+    assert(find(array, (i) => i > 1) === 2)
   })
 
   it('returns undefined', function () {
-    assert(find(array, (i) => i === 69) === undefined)
+    assert(find(array, (i) => i === 420) === undefined)
   })
 })
