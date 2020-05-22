0ded7331a25970bcef38c35a02d5c34afe153577
Eduard Tsech <edtsech@gmail.com>
2020-05-22 10:27:59 +0200

    Add complexity test for find

--- src/find/test.ts
+++ src/find/test.ts
@@ -1,11 +1,23 @@
 import assert from 'assert'
 import find from '.'
+import sinon from 'sinon'
 
 describe('find', function () {
   var array = [1, 2, 3]
 
   it('finds first element by condition', function () {
     assert(find(array, (i) => i > 1) === 2)
+  })
+
+  it('stops execution as soon as it finds the element', function () {
+    sinon.stub()
+    const matcher = sinon.stub()
+    matcher.withArgs(1).returns(false)
+    matcher.withArgs(2).returns(true)
+    matcher.withArgs(3).returns(true)
+
+    assert(find(array, matcher) === 2)
+    assert(matcher.callCount === 2)
   })
 
   it('returns undefined', function () {
