92d8cbc88ea43e78a8d0f96ad6471fd4d361d23c
Sasha Koss <koss@nocorp.me>
2020-05-24 20:28:00 +0400

    Add sample function (#23)

---
+++ src/sample/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Returns a random element from the given array.
+ *
+ * @param array - The array to get element from
+ * @returns A random element
+ *
+ * @public
+ */
+export default function sample<ElementType>(
+  array: Array<ElementType>
+): ElementType | undefined {
+  const randomIndex = Math.round(Math.random() * (array.length - 1))
+  return array[randomIndex]
+}
---
+++ src/sample/test.ts
@@ -0,0 +1,16 @@
+import assert from 'assert'
+import sample from '.'
+
+describe('sample', function () {
+  var array = [1, 2, 3]
+
+  it('returns random element from the given array', function () {
+    const result = sample(array)
+    assert(array.indexOf(result) !== -1)
+  })
+
+  it('returns undefined if the array is empty', function () {
+    const result = sample([])
+    assert(result === undefined)
+  })
+})
