722e882b7243ea7988bae9bbb108fd2a46e8bceb
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2020-06-16 18:04:08 +0530

    add tighter test for values

--- src/values/test.ts
+++ src/values/test.ts
@@ -6,6 +6,15 @@ describe('values', () => {
     assert.deepEqual(values({ a: 1, b: 2, c: 3 }), [1, 2, 3])
   })
 
+  it('only returns the properties of the given object and not inherited ones', () => {
+    const obj1 = { a: 1 }
+    const obj2 = Object.create(obj1)
+    obj2.b = 2
+    obj2.c = 3
+
+    assert.deepEqual(values(obj2), [2, 3])
+  })
+
   it('returns an empty array for an empty object', () => {
     assert.deepEqual(values({}), [])
   })
